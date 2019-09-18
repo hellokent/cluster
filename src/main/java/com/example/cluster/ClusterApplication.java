f0e7ed4568d2f246a4d4bf7387836f14443f2b66
陈阳 <chenyang.coder@gmail.com>
2019-09-18 20:08:16 +0800

    git ignore

---
+++ src/main/java/com/example/cluster/ClusterApplication.java
@@ -0,0 +1,10 @@
+package com.example.cluster;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterApplication {
+    public static void main(String[] args) {
+
+    }
+}
