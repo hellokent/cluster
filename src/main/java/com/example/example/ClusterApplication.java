440043c6945eeaf5bee6918f28054bdfccb6a487
chenyang <chenyang@zhihu.com>
2019-09-18 19:01:41 +0800

    init

---
+++ src/main/java/com/example/example/ClusterApplication.java
@@ -0,0 +1,10 @@
+package com.example.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterApplication {
+    public static void main(String[] args) {
+
+    }
+}
